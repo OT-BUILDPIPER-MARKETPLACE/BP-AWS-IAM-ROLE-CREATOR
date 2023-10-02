7d737a0d6adb8e37ecb6d5aa6988b9af32dbb246
Sandeep <sandeep@opstree.com>
2023-10-02 21:36:56 +0530

    Added capability for role validation

--- build.sh
+++ build.sh
@@ -10,8 +10,15 @@ sleep  $SLEEP_DURATION
 
 TASK_STATUS=0
 
-envsubst < assume-role-policy.template > assume-role-policy
+ROLE_EXISTS=`roleExists ${ROLE_NAME}`
 
-createRole ${ROLE_NAME} assume-role-policy
+if [ "$ROLE_EXISTS" -eq 0 ]
+then
+    logWarningMessage "Role with name ${ROLE_NAME} already exists"
+else
+    logInfoMessage "Creating Role, as it already doesn't exists"
+    envsubst < assume-role-policy.template > assume-role-policy
+    createRole ${ROLE_NAME} assume-role-policy
+fi
 
 saveTaskStatus ${TASK_STATUS} ${ACTIVITY_SUB_TASK_CODE}
